612892eb07f0618e60577c14fd227424327e31cb
mpgxc <mpgx5.c@gmail.com>
2020-11-05 04:37:36 -0300

    feat: add sonic routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
+
 import postRouter from "./post.routes";
 import usersRouter from "./users.routes";
 import sessionRouter from "./session.routes";
+import sonicRouter from "./sonic.routes";
 
 const routes = Router();
 
 routes.use("/user", usersRouter);
 routes.use("/post", postRouter);
 routes.use("/session", sessionRouter);
+routes.use("/sonic", sonicRouter);
 
 export default routes;
