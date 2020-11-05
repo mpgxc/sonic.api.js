14a794b0353b7feb12b64ea2f4367b292674fc4f
mpgxc <mpgx5.c@gmail.com>
2020-11-05 02:56:02 -0300

    feat: call controller posts on routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import postRouter from "./post.routes";
 import usersRouter from "./users.routes";
 import sessionRouter from "./session.routes";
 
 const routes = Router();
 
 routes.use("/user", usersRouter);
+routes.use("/post", postRouter);
 routes.use("/session", sessionRouter);
 
 export default routes;
