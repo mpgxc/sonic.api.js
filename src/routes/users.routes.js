26a566e0816d7d7aac68dce406c635a871003515
mpgxc <mpgx5.c@gmail.com>
2020-11-05 02:07:57 -0300

    feat: apply middleware to user routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import UserController from "../controllers/User.Controller";
+import ensureAuthenticate from "../middlewares/ensureAuthenticate";
 
 const userRouter = Router();
 
-userRouter.get("/", UserController.index);
+userRouter.get("/", ensureAuthenticate, UserController.index);
 userRouter.post("/", UserController.create);
 
 export default userRouter;
